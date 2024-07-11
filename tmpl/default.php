7a96b725068634f5afeaf5105f83e6a50343ec1e
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2024-07-11 22:23:55 +0100

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,11 +8,25 @@
  * @link       [AUTHOR_URL]
  */
 
+// No direct access
 defined('_JEXEC') or die;
-
-$domain = $params->get('domain', 'https://www.joomla.org');
 ?>
 
-<a href="<?php echo $domain; ?>">
-	<?php echo 'JOOMCraft ' . $test; ?>
-</a>
+<div class="joomcraft<?php echo $moduleclass_sfx; ?>">
+    <div>
+        <img src="https://cdn.pixabay.com/photo/2013/07/12/19/25/minecraft-154749_1280.png" id="server_icon" />
+        <div>
+            <h1 id="server_ip"><?php echo $server_name; ?></h1>
+            <h1 id="is_online">STATUS</h1>
+            <span id="motd">A beautiful minecraft server</span>
+            <div><span id="player_count">x</span> of <span id="player_max">y</span> Players Online</div>
+        </div>
+    </div>
+    
+    <script src="<?php echo JUri::base() . 'modules/mod_joomcraft/assets/script.js'; ?>"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            initServerData("<?php echo $server_name; ?>", "<?php echo $server_port; ?>");
+        });
+    </script>
+</div>
