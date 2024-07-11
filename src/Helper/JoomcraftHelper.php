2787e8b3dfd6383e3b41ed0f2c02d9f6cd77b978
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2024-07-11 23:43:17 +0100

    Update JoomcraftHelper.php

--- src/Helper/JoomcraftHelper.php
+++ src/Helper/JoomcraftHelper.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  mod_joomcraft
  *
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 3; see LICENSE.txt
  */
 
 namespace JoomcraftNamespace\Module\Joomcraft\Site\Helper;
@@ -25,13 +25,20 @@ class JoomcraftHelper
     public static function getText()
     {
         $result       = [];
-        $is_online    = false;
-        $motd         = '';
-        $player_count = 0;
-        $player_max   = 0;
-        $server_icon  = '';
+        $is_online    = true; // Example value, replace with actual logic to determine online status
+        $motd         = 'Welcome to our server'; // Example message of the day
+        $player_count = 20; // Example number of players online
+        $player_max   = 100; // Example maximum players allowed
+        $server_icon  = ''; // Example server icon data (binary)
 
-        // Simulate some data for example purposes
+        // Simulate retrieving binary image data
+        $binaryImageData = file_get_contents('path/to/server_icon.png'); // Replace with actual retrieval logic
+
+        if ($binaryImageData) {
+            // Convert binary data to base64 format
+            $server_icon = 'data:image/png;base64,' . base64_encode($binaryImageData);
+        }
+
         $result['is_online']    = $is_online;
         $result['motd']         = $motd;
         $result['player_count'] = $player_count;
