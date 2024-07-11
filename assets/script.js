41a63952acb83b14597b552513a7246eca617827
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2024-07-11 22:32:46 +0100

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+function initServerData(serverIp, serverPort) {
+    const serverIpElement = document.getElementById('server_ip');
+    serverIpElement.innerHTML = serverIp;
+    fetch('https://mcapi.us/server/status?ip=' + serverIp + '&port=' + serverPort)
+        .then(response => response.json())
+        .then(data => handleServerStatus(data));
+}
+
+function handleServerStatus(data) {
+    if (data.status == 'error') {
+        console.log(data.error);
+        return false;
+    }
+
+    const isOnline = document.getElementById("is_online");
+    isOnline.innerHTML = data.online;
+
+    const motd = document.getElementById("motd");
+    motd.innerHTML = data.motd;
+
+    const playerCounter = document.getElementById("player_count");
+    playerCounter.innerHTML = data.players.now;
+
+    const playerCounterMax = document.getElementById("player_max");
+    playerCounterMax.innerHTML = data.players.max;
+
+    const logo = document.getElementById("server_icon");
+    logo.src = data.favicon;
+}
