d98fbd45f20f9412ec9be970c974f111b529e7ad
nodenetworks <51421180+nodenetworks@users.noreply.github.com>
2024-07-14 23:58:26 +0100

    Added switches

--- tmpl/default.php
+++ tmpl/default.php
@@ -14,6 +14,10 @@ defined('_JEXEC') or die;
 // Get parameters
 $server_name = $params->get('server_name');
 $server_port = $params->get('server_port');
+$show_server_icon = $params->get('show_server_icon', 1);
+$show_is_online = $params->get('show_is_online', 1);
+$show_motd = $params->get('show_motd', 1);
+$show_player_count = $params->get('show_player_count', 1);
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
 // Load language strings into JavaScript
@@ -23,19 +27,25 @@ JText::script('MOD_JOOMCRAFT_OFFLINE');
 
 <div class="joomcraft<?php echo $moduleclass_sfx; ?>">
     <div>
-        <img id="server_icon" />
+        <?php if ($show_server_icon): ?>
+            <img id="server_icon" />
+        <?php endif; ?>
         <div>
             <h1 id="server_ip"><?php echo $server_name; ?></h1>
-            <h1 id="is_online">
-                <?php echo JText::_('MOD_JOOMCRAFT_STATUS_LOADING'); ?>
-            </h1>
-            <span id="motd"><?php echo $serverData['motd']; ?></span>
-            <div>
-                <span id="player_count"><?php echo $serverData['player_count']; ?></span>
-                <?php echo JText::_('MOD_JOOMCRAFT_OF'); ?>
-                <span id="player_max"><?php echo $serverData['player_max']; ?></span>
-                <?php echo JText::_('MOD_JOOMCRAFT_PLAYERS_ONLINE'); ?>
-            </div>
+            <?php if ($show_is_online): ?>
+                <h1 id="is_online"><?php echo JText::_('MOD_JOOMCRAFT_STATUS_LOADING'); ?></h1>
+            <?php endif; ?>
+            <?php if ($show_motd): ?>
+                <span id="motd"><?php echo $serverData['motd']; ?></span>
+            <?php endif; ?>
+            <?php if ($show_player_count): ?>
+                <div>
+                    <span id="player_count"><?php echo $serverData['player_count']; ?></span>
+                    <?php echo JText::_('MOD_JOOMCRAFT_OF'); ?>
+                    <span id="player_max"><?php echo $serverData['player_max']; ?></span>
+                    <?php echo JText::_('MOD_JOOMCRAFT_PLAYERS_ONLINE'); ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
     
@@ -47,7 +57,9 @@ JText::script('MOD_JOOMCRAFT_OFFLINE');
             // Set the server icon
             var serverIcon = '<?php echo $serverData['server_icon']; ?>';
             var serverIconImg = document.getElementById('server_icon');
-            serverIconImg.src = serverIcon;
+            if (serverIconImg) {
+                serverIconImg.src = serverIcon;
+            }
         });
     </script>
 </div>
